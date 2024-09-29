3b364396bad782a40a55bdcda1eba044560473bd
Wojtala <pawel.wojtala@aptiv.com>
2024-09-29 02:27:04 +0200

    Working extension download

--- frontend/src/extension-proposal.tsx
+++ frontend/src/extension-proposal.tsx
@@ -7,6 +7,6 @@ export default function ExtensionProposal(): JSX.Element {
         }}>
         <Typography variant='body1'>Like it? Get our browser plug-in and stay safe!</Typography>
         <div style={{paddingLeft: '0.5em'}}>
-        <Button variant='outlined' size='small'>Get plug-in</Button></div>
+        <Button variant='outlined' size='small' href={`${process.env.PUBLIC_URL}/extension.zip`}>Get plug-in</Button></div>
         </div>
 }
