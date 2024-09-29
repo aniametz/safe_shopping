411c1d76d6c508f3938399eeec70bf817765f126
Piotr Weszka <piotr.weszka@aptiv.com>
2024-09-29 03:37:43 +0200

    ec2 url update

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const backend_port = "http://ec2-35-181-50-227.eu-west-3.compute.amazonaws.com";
+export const backend_port = "http://ec2-35-181-50-227.eu-west-3.compute.amazonaws.com/";
 
 export interface MarkerType {
   label: string;
