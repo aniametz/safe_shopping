c5934e7b6f3418c4877dfd07f0a4498ac86eefb8
Piotr Weszka <piotr.weszka@aptiv.com>
2024-09-29 03:22:40 +0200

    Changed api url to ec2

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const backend_port = "http://127.0.0.1:5000/";
+export const backend_port = "http://ec2-35-181-50-227.eu-west-3.compute.amazonaws.com";
 
 export interface MarkerType {
   label: string;
